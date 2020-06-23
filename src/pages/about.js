caba2f2ab6365cd548c3d3524266ef395fcc7e3a
Joseph Butterfield <raleigh9123@gmail.com>
2020-06-23 01:06:31 -0700

    Gatsby Site Hello World Starter
    Completed Section 1. of tutorial. and initialized starter gatsby site "Hello World"

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/header"
+
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header headerText="About Gatsby">About Gatsby</Header>
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
